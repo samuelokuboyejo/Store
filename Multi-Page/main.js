8d734e4cd0b5c7a910007d9d28f6b25eb23e4186
Samuel Okuboyejo <78604165+samuelokuboyejo@users.noreply.github.com>
2022-08-22 21:40:36 +0100

    Add files via upload

--- Multi-Page/main.js
+++ Multi-Page/main.js
@@ -37,3 +37,24 @@ var swiper = new Swiper(".mySwiper", {
         },
     },
 });
+
+// Show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+});
+
+// close nav menu button
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-lock";
+};
+
+closeBtn.addEventListener("click", closeNav);
